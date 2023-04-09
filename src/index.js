7b5d359a2fa878490c972c9a53b4d25ee6e4e58f
Tyson Jones <ballisticmissle@gmail.com>
2023-04-09 07:28:28 -0600

    d

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 
 const date = new Date();
 if (calculateEaster(date.getFullYear()).toDateString() === date.toDateString()) {
-    main.innerText = "Yes.";
+    main.innerText = "Yes";
 } else {
-    main.innerText = "No.";
+    main.innerText = "No";
 }
